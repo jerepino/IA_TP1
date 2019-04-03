6ee4cf9ee52b43496eee24b6042c5b8989ba73a4
Jeremias Pino <jerepino@gmail.com>
2019-04-03 12:58:25 -0300

    AG terminado, falta testear solucion

--- ej4.py
+++ ej4.py
@@ -19,19 +19,23 @@ def fitness(individuo, lista_productos, lista_ordenes, mapa):
 
     for index in range(0,len(lista_productos)):
         lista_productos[index].ubicacion = individuo[index]
-    E = 0
+    f = 0
     estado_actual = []
+    # print(individuo)
     for index, ordenes in enumerate(lista_ordenes):
         for ind, orden in enumerate(ordenes):
             for lugar, producto in enumerate(lista_productos):
                 if orden.tipo == producto.tipo:
                     orden.ubicacion = producto.ubicacion
-
+                    # print("orden ---- producto")
+                # print(orden.tipo, orden.ubicacion, producto.tipo, producto.ubicacion)
             estado_actual.append(copy.deepcopy(orden.ubicacion))
-        solucion=simulated_annealing.recocido_simulado(simulated_annealing.Nodo(estado_actual), mapa)
-        E = E + solucion.E
+        # print("estado actual")
+        # print(estado_actual)
+        solucion = simulated_annealing.recocido_simulado(simulated_annealing.Nodo(estado_actual), mapa)
+        f = f + solucion.E
         estado_actual.clear()
-    f = 1000 / E
+
     return f
 
 
@@ -48,28 +52,25 @@ def AG(tamaño_poblacion,cant_interaciones, lista_productos, lista_ordenes, mapa
         random.shuffle(semilla)
         poblacion.append(copy.deepcopy(Individuo(semilla)))
 
-
-
+    vector_fitness = []
     while k < cant_interaciones:
         #Calculo la idoneidad de mi poblacion
 
-        # for index, item in enumerate(poblacion):
-        #     poblacion[index].idoneidad = fitness(item.estado, lista_productos, lista_ordenes, mapa)
-
-        poblacion.sort(key=lambda Individuo: Individuo.idoneidad, reverse=False)
-
-
+        promedio_fitness = 0
+        for index, item in enumerate(poblacion):
+            poblacion[index].idoneidad = fitness(item.estado, lista_productos, lista_ordenes, mapa)
+            promedio_fitness = promedio_fitness + poblacion[index].idoneidad
+        promedio_fitness = promedio_fitness /tamaño_poblacion
+        poblacion.sort(key=lambda Individuo: Individuo.idoneidad)
+        vector_fitness.append(promedio_fitness)
+        print(promedio_fitness)
         del poblacion[int(tamaño_poblacion/2+1):]#elijo los n/2 mejores padres
 
         nueva_generacion = []
         #Realizo crossover debo cambiarlo
         for i in range(0, len(poblacion)-1):
-            # nueva_generacion.append(copy.deepcopy(poblacion[i]))
-            # nueva_generacion[i].estado.clear()
-            # nueva_generacion.append(copy.deepcopy(poblacion[i+1]))
-            # nueva_generacion[i+1].estado.clear()
-            index_1 = random.randint(0, len(poblacion[i].estado))
-            index_2 = random.randint(0, len(poblacion[i].estado))
+            index_1 = random.randint(1, len(poblacion[i].estado)-1)
+            index_2 = random.randint(1, len(poblacion[i].estado)-1)
             if index_1 > index_2:
                 auxx = index_1
                 index_1 = index_2
@@ -81,55 +82,62 @@ def AG(tamaño_poblacion,cant_interaciones, lista_productos, lista_ordenes, mapa
             hijo_2 = []
             hijo_1 = poblacion[i+1].estado[index_1:index_2]#[index_1, index_2)
             hijo_2 = poblacion[i].estado[index_1:index_2]
-            print(hijo_1)
-            print(hijo_2)
-            estado_aux_derecha = [n for n in (poblacion[i].estado) if n not in hijo_1] #incluye index_2
-            estado_aux_derecha_2 = [n for n in (poblacion[i + 1].estado) if n not in hijo_2]  # incluye index_2
-            if index_2 != 8 and index_1 != 0:
-                estado_aux_izquierda = estado_aux_derecha[len(estado_aux_derecha[index_1:]):] #(index2,fin]
-                estado_aux_izquierda_2 = estado_aux_derecha_2[len(estado_aux_derecha_2[index_1:]):]
-                del estado_aux_derecha[len(estado_aux_derecha[index_1:]):]
-                del estado_aux_derecha_2[len(estado_aux_derecha_2[index_1:]):]
-                hijo_1 = estado_aux_izquierda + hijo_1 + estado_aux_derecha
-                hijo_2 = estado_aux_izquierda_2 + hijo_2 + estado_aux_derecha_2
-            elif index_2 == 8:
-                hijo_1 = estado_aux_derecha + hijo_1
-                hijo_2 = estado_aux_derecha_2 + hijo_2
-            else:
-                hijo_1 = hijo_1 + estado_aux_derecha
-                hijo_2 = hijo_2 + estado_aux_derecha
+            # print(hijo_1)
+            # print(hijo_2)
+            estado_aux_derecha = [n for n in poblacion[i].estado if n not in hijo_1] #incluye index_2
+            estado_aux_derecha_2 = [n for n in poblacion[i + 1].estado if n not in hijo_2]  # incluye index_2
+
+            estado_aux_izquierda = estado_aux_derecha[len(estado_aux_derecha[index_1:]):] #[index2,fin]
+            estado_aux_izquierda_2 = estado_aux_derecha_2[len(estado_aux_derecha_2[index_1:]):]
+            del estado_aux_derecha[len(estado_aux_derecha[index_1:]):]
+            del estado_aux_derecha_2[len(estado_aux_derecha_2[index_1:]):]
+            hijo_1 = estado_aux_izquierda + hijo_1 + estado_aux_derecha
+            hijo_2 = estado_aux_izquierda_2 + hijo_2 + estado_aux_derecha_2
 
             nueva_generacion.append(Individuo(copy.deepcopy(hijo_1)))
             nueva_generacion.append(Individuo(copy.deepcopy(hijo_2)))
             hijo_2.clear()
             hijo_1.clear()
+            estado_aux_derecha.clear()
+            estado_aux_derecha_2.clear()
 
-            print("crossover")
-            print(index_1, index_2)
-            print("padres")
-            print(poblacion[i].estado)
-            print(poblacion[i+1].estado)
-            print("hijo_1")
-            print(nueva_generacion[i*2].estado)
-            print("hijo_2")
-            print(nueva_generacion[(2*i+1)].estado)
-            print("valores izq_, derecha_")
-            print( estado_aux_izquierda,estado_aux_derecha)
+            estado_aux_izquierda.clear()
+            estado_aux_izquierda_2.clear()
 
-        #Mutaciones con probabilidad del 15%
+            # print("crossover")
+            # print(index_1, index_2)
+            # print("padres")
+            # print(poblacion[i].estado)
+            # print(poblacion[i+1].estado)
+            # print("hijo_1")
+            # print(nueva_generacion[i*2].estado)
+            # print("hijo_2")
+            # print(nueva_generacion[(2*i+1)].estado)
+            # print("valores izq_, derecha_")
+            # print( estado_aux_izquierda,estado_aux_derecha)
 
-        for index, intem in enumerate(nueva_generacion):
-            if random.random() < 0.15:
-                index_1 = random.randint(1, len(item.estado) - 1)
-                index_2 = random.randint(1, len(item.estado) - 1)
-                aux = copy.deepcopy(item.estado[index_1])
-                nueva_generacion[index].estado[index_1] = copy.deepcopy(item.estado[index_2])
-                nueva_generacion[index].estado[index_2] = aux
-        print(len(poblacion), len(nueva_generacion))
+        #Mutaciones por Intercambio con probabilidad del 15%
+
+        for index, hijo in enumerate(nueva_generacion):
+            # print("Epoca de mutacion")
+            if random.random() < 0.01:
+                # print("El individuo ", index, "esta mutando")
+                # print(hijo.estado)
+                pos_1 = random.randint(0, len(hijo.estado) - 1)
+                pos_2 = random.randint(0, len(hijo.estado) - 1)
+                if pos_1 == pos_2:
+                    pos_1 = random.randint(0, len(hijo.estado) - 1)
+                auxiliar = hijo.estado[pos_1]
+                nueva_generacion[index].estado[pos_1] = hijo.estado[pos_2]
+                nueva_generacion[index].estado[pos_2] = auxiliar
+                # print("El resultado de la mutacion es")
+                # print(nueva_generacion[index].estado)
+        # print(len(poblacion), len(nueva_generacion))
         poblacion.clear()
         poblacion = copy.deepcopy(nueva_generacion)
         nueva_generacion.clear()
         k+=1
+    print(vector_fitness)
     return poblacion[0]
 
 
@@ -175,7 +183,7 @@ def main():
                 print(valo.tipo, end=' -> ')
         print("]")
 
-    solucion = AG(10, 20, lista_productos, lista_ordenes, mapa)
-    print(solucion.estado)
+    solucion = AG(20, 20, lista_productos, lista_ordenes, mapa)
+    print(solucion.estado, solucion.idoneidad)
 if __name__ == '__main__':
            main()
